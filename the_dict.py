82127dbacc0f081dc5b044b37038a494d4b26554
lifeofmine <uccent@sina.com>
2018-05-04 17:23:04 +0800

    the dict and the set

---
+++ the_dict.py
@@ -0,0 +1,10 @@
+# python
+# -*-coding:utf-8 -*-
+# time :2018/5/4 17:17
+# @Author: 50433
+# @File:.py
+
+d = {'tuple': (1, 3, 5), 'list': [1, 3, 5]}
+print(d['tuple'], d['list'])
+print(d['tuple'][-1])
+print(d['list'][1])
---
+++ the_set.py
@@ -0,0 +1,10 @@
+# python
+# -*-coding:utf-8 -*-
+# time :2018/5/4 17:21
+# @Author: 50433
+# @File:.py
+
+d = set((1, 3, (1, 5, 6)))
+print(d)
+d = set([1, 2])
+print(d)
