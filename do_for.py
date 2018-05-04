ad9d445c3395cb43d656a286640db6fcd20cdafb
lifeofmine <uccent@sina.com>
2018-05-04 17:02:18 +0800

    do for and do while

---
+++ do_for.py
@@ -0,0 +1,9 @@
+# python
+# -*-coding:utf-8 -*-
+# time :2018/5/4 16:56
+# @Author: 50433
+# @File:.py
+sum = 0
+for x in range(101):
+    sum += x
+print(sum)
---
+++ do_while.py
@@ -0,0 +1,12 @@
+# python
+# -*-coding:utf-8 -*-
+# time :2018/5/4 16:58
+# @Author: 50433
+# @File:.py
+L = ['Bart', 'Lisa', 'Adam']
+n = 0
+while n < len(L):
+    print(L[n], "\n")
+    n = n + 1
+
+
