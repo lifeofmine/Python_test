597f770f783aeb2ab65db2d17b129c55f982ce25
lifeofmine <uccent@sina.com>
2018-05-04 16:43:49 +0800

    the list

---
+++ the_list.py
@@ -0,0 +1,14 @@
+# python
+# -*-coding:utf-8 -*-
+# time :2018/5/4 16:41
+# @Author: 50433
+# @File:.py
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    'Adam', 'Bart', 'Lisa'
+]
+print(L[0][0])
+print(L[1][1])
+print(L[-1])
